5249891d0c95d51cc4d080b942e3715ea3d351ed
John Need <johnneed@johnneed.com>
2020-02-12 09:53:27 -0500

    d3 added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-window.alert("foo");
+import * as d3 from "d3";
+
+
+var data = [30, 86, 168, 281, 303, 365];
+
+d3.select(".chart")
+  .selectAll("div")
+  .data(data)
+  .enter()
+  .append("div")
+  .style("width", function (d) {
+    return d + "px";
+  })
+  .text(function (d) {
+    return d;
+  });
